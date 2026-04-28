72273e255855d377203f5bdde4217ec47f3c7202
Oxnack <128256361+Oxnack@users.noreply.github.com>
2026-04-28 11:16:55 +0300

    sm

---
+++ site/main.js
@@ -0,0 +1,7 @@
+// Простейший скрипт для переключения табов
+document.querySelectorAll('.nav-item').forEach(item => {
+    item.addEventListener('click', function() {
+        document.querySelector('.nav-item.active').classList.remove('active');
+        this.classList.add('active');
+    });
+});
